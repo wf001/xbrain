adc6157b546efdc2657330c6e79685fe563d1167
wf001 <wf001@diax.xyz>
2021-02-03 21:03:34 +0900

    Fix remove optimization process.

--- bf-x86.c
+++ bf-x86.c
@@ -167,208 +167,6 @@ void program_parse(struct program *p, FILE *in) {
     program_add(p, INS_HALT, 0);
 }
 
-void program_strip_nops(struct program *p) {
-    struct program copy = PROGRAM_INIT;
-    for (size_t i = 0; i < p->count; i++)
-        if (p->ins[i].ins != INS_NOP)
-            program_add(&copy, p->ins[i].ins, p->ins[i].operand);
-    program_free(p);
-    *p = copy;
-}
-
-enum optype { OP_ANY, OP_CONSTANT, OP_STORE, OP_LOAD, OP_LOAD_NEG };
-
-struct pattern {
-    enum ins ins;
-    enum optype optype;
-    long operand;
-};
-
-struct optimization {
-    const char *name;
-    struct pattern *sequence;
-    struct pattern *replacement;
-};
-
-void optimize_replace(struct program *p, long i, struct optimization *o,
-                      long *vars) {
-    for (long n = 0; o->sequence[n].ins != INS_NOP; n++) {
-        p->ins[i + n].ins = o->replacement[n].ins;
-        switch (o->replacement[n].optype) {
-            case OP_CONSTANT:
-                p->ins[i + n].operand = o->replacement[n].operand;
-                break;
-            case OP_LOAD:
-                p->ins[i + n].operand = vars[o->replacement[n].operand];
-                break;
-            case OP_LOAD_NEG:
-                p->ins[i + n].operand = -vars[o->replacement[n].operand];
-                break;
-            case OP_ANY:
-                break;
-            case OP_STORE:
-                assert(!"invalid replacement");
-                break;
-        }
-    }
-}
-
-long optimize_apply(struct program *p, struct optimization *o) {
-    long count = 0;
-    long variables[16] = {0};
-    for (size_t i = 0; i < p->count; i++) {
-        long length = 0;
-        for (size_t n = 0; o->sequence[n].ins != INS_NOP && i + n < p->count;
-             n++) {
-            enum ins ins = p->ins[i + n].ins;
-            long operand = p->ins[i + n].operand;
-            if (ins != o->sequence[n].ins) {
-                length = 0;
-                break;
-            }
-            length++;
-            enum optype o_type = o->sequence[n].optype;
-            long o_operand = o->sequence[n].operand;
-            switch (o_type) {
-                case OP_CONSTANT:
-                    if (operand != o_operand) length = 0;
-                    break;
-                case OP_STORE:
-                    variables[o_operand] = operand;
-                    break;
-                case OP_LOAD:
-                    if (operand != variables[o_operand]) length = 0;
-                    break;
-                case OP_LOAD_NEG:
-                    if (operand != -variables[o_operand]) length = 0;
-                    break;
-                case OP_ANY:
-                    break;
-            }
-            if (length == 0) break;
-        }
-        if (length > 0) {
-            optimize_replace(p, i, o, variables);
-            i += length;
-            count++;
-        }
-    }
-    if (count > 0) program_strip_nops(p);
-    return count;
-}
-
-/* [-] */
-struct optimization optimization_clear = {
-    .name = "Cell clear",
-    .sequence =
-        (struct pattern[]){
-            {INS_BRANCH}, {INS_MUTATE, OP_CONSTANT, -1}, {INS_JUMP}, {INS_NOP}},
-    .replacement = (struct pattern[]){{INS_CLEAR}, {INS_NOP}, {INS_NOP}}};
-
-/* [->+<] */
-struct optimization optimization_add = {
-    .name = "Cell add",
-    .sequence = (struct pattern[]){{INS_BRANCH},
-                                   {INS_MUTATE, OP_CONSTANT, -1},
-                                   {INS_MOVE, OP_STORE, 0},
-                                   {INS_MUTATE, OP_CONSTANT, 1},
-                                   {INS_MOVE, OP_LOAD_NEG, 0},
-                                   {INS_JUMP},
-                                   {INS_NOP}},
-    .replacement = (struct pattern[]){{INS_ADD, OP_LOAD, 0},
-                                      {INS_CLEAR},
-                                      {INS_NOP},
-                                      {INS_NOP},
-                                      {INS_NOP},
-                                      {INS_NOP}}};
-
-void program_compress(struct program *p) {
-    for (size_t i = 0; i < p->count; i++) {
-        if (p->ins[i].ins == INS_MOVE || p->ins[i].ins == INS_MUTATE) {
-            size_t f = i + 1;
-            while (p->ins[i].ins == p->ins[f].ins) {
-                p->ins[f].ins = INS_NOP;
-                p->ins[i].operand += p->ins[f].operand;
-                f++;
-            }
-            if (p->ins[i].operand == 0) p->ins[i].ins = INS_NOP;
-        }
-    }
-    program_strip_nops(p);
-}
-
-void program_optimize(struct program *p, int level) {
-    if (level == 0) return;
-    program_compress(p);
-    long count = 0;
-    do {
-        count = 0;
-        if (level >= 2) count += optimize_apply(p, &optimization_clear);
-        if (level >= 3) count += optimize_apply(p, &optimization_add);
-    } while (count > 0);
-}
-
-void program_print(const struct program *p, FILE *out) {
-    for (size_t i = 0; i < p->count; i++) {
-        long operand = p->ins[i].operand;
-        enum ins ins = p->ins[i].ins;
-        if (ins != INS_NOP) {
-            fprintf(out, "%08ld  ", i);
-            if (instruction_arity(ins) == 1)
-                fprintf(out, "%-12s%ld\n", instruction_name(ins), operand);
-            else
-                fprintf(out, "%s\n", instruction_name(ins));
-        }
-    }
-}
-
-void interpret(const struct program *, FILE *);
-
-void interpret(const struct program *program, FILE *trace) {
-    long ip = 0;
-    long dp = 0;
-    uint8_t memory[MEMORY_SIZE] = {0};
-    for (;;) {
-        enum ins ins = program->ins[ip].ins;
-        long operand = program->ins[ip].operand;
-        if (trace != NULL && ins != INS_NOP) {
-            const char *name = instruction_name(ins);
-            fprintf(trace, "(%ld) %s %ld\n", ip, name, operand);
-        }
-        ip++;
-        switch (ins) {
-            case INS_MOVE:
-                dp += operand;
-                break;
-            case INS_MUTATE:
-                memory[dp] += operand;
-                break;
-            case INS_IN:
-                memory[dp] = getchar();
-                break;
-            case INS_OUT:
-                putchar(memory[dp]);
-                break;
-            case INS_JUMP:
-                ip = operand;
-                break;
-            case INS_BRANCH:
-                if (memory[dp] == 0) ip = operand;
-                break;
-            case INS_CLEAR:
-                memory[dp] = 0;
-                break;
-            case INS_ADD:
-                memory[dp + operand] += memory[dp];
-                break;
-            case INS_NOP:
-                break;
-            case INS_HALT:
-                return;
-        }
-    }
-}
-
 struct asmbuf {
     size_t size, fill;
     uint8_t code[];
@@ -587,13 +385,9 @@ void elf_write(struct asmbuf *buf, FILE *elf) {
 }
 
 void print_help(const char *argv0, FILE *o) {
-    fprintf(o, "Usage: %s [-o <file>] [-i] [-e] [-h] [-O <n>]\n", argv0);
+    fprintf(o, "Usage: %s [-o <file>] [-h] \n", argv0);
     fprintf(o, "  -o <file>    executable output file name\n");
-    fprintf(o, "  -O <level>   optimization level\n");
-    fprintf(o, "  -e           no output file, execute program\n");
-    fprintf(o, "  -i           no output file, interpret program (slow)\n");
     fprintf(o, "  -h           print this usage information\n");
-    fprintf(o, "  -D           debugging listing\n");
 }
 
 int main(int argc, char **argv) {
@@ -601,9 +395,6 @@ int main(int argc, char **argv) {
     program_name = argv[0];
     char *output = NULL;
     char *input = NULL;
-    bool do_exec = false;
-    bool do_interpret = false;
-    bool do_debug = false;
     int optimize = 3;
 
     /* Parse arguments */
@@ -612,21 +403,6 @@ int main(int argc, char **argv) {
         switch (option) {
             case 'o':
                 output = optarg;
-                break;
-            case 'O':
-                optimize = atoi(optarg);
-                break;
-            case 'e':
-                do_exec = true;
-                break;
-            case 'i':
-                do_interpret = true;
-                break;
-            case 'D':
-                do_debug = true;
-                break;
-            case 'h':
-                print_help(argv[0], stdout);
                 break;
             default:
                 print_help(argv[0], stderr);
@@ -640,7 +416,7 @@ int main(int argc, char **argv) {
     input = argv[optind];
 
     char output_buf[1024];
-    if (!do_interpret && !do_exec && output == NULL) {
+    if (output == NULL) {
         snprintf(output_buf, sizeof(output_buf), "%s", input);
         output = output_buf;
         char *p = output + strlen(output);
@@ -655,27 +431,15 @@ int main(int argc, char **argv) {
     FILE *source = fopen(argv[optind], "r");
     if (source == NULL) FATAL("could not open input file");
     program_parse(&program, source);
-    program_optimize(&program, optimize);
     fclose(source);
 
-    if (do_debug) program_print(&program, stderr);
-
-    if (do_interpret) {
-        interpret(&program, do_debug ? stderr : NULL);
-    } else if (do_exec) {
-        struct asmbuf *buf = compile(&program, MODE_FUNCTION);
-        void (*run)(void) = (void *)buf->code;
-        run();
-        asmbuf_free(buf);
-    } else {
-        struct asmbuf *buf = compile(&program, MODE_STANDALONE);
-        FILE *elf = fopen(output, "wb");
-        if (elf == NULL) FATAL("could not open output file");
-        elf_write(buf, elf);
-        fchmod(fileno(elf), 0755);
-        fclose(elf);
-        asmbuf_free(buf);
-    }
+    struct asmbuf *buf = compile(&program, MODE_STANDALONE);
+    FILE *elf = fopen(output, "wb");
+    if (elf == NULL) FATAL("could not open output file");
+    elf_write(buf, elf);
+    fchmod(fileno(elf), 0755);
+    fclose(elf);
+    asmbuf_free(buf);
 
     program_free(&program);
     return 0;
