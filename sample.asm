502dfc4b8fb1ec37ef07056721e781a200ce209e
wf001 <tarkiew@gmail.com>
2021-02-09 00:57:25 +0900

    Fix make assembly code to do `and` ope, and print hex as it is.

--- sample.asm
+++ sample.asm
@@ -3,14 +3,47 @@ section .text
 global _start
 
 _start:
-    mov rax, 0x72
-    and rax, 0x24
-    add rax, 0x18
-    push rax
+    mov rax, 0x4C
+    and rax, 0x2E
+    mov r11, rax
+    jmp .loop
+
+    
+.loop:
+    cmp rax, 0x0
+    jz .print
+    and r11, 0xf
+    cmp r11, 0xa
+    jae .a
+    jb .b
+
+.a:
+    add r11, 0x37
+    jmp .c
+.b:
+    add r11, 0x30
+    jmp .c
+
+.c
+    push r11
+    tar rax, 4
+    mov r11, rax
+    inc r12
+    jmp .loop
+
+.print:
     mov rsi, rsp
     mov rdi, 0x1
     mov rax, 0x1
     mov rdx, 0x1
     syscall
+    pop r13
+
+    dec r12
+    cmp r12, 0x0
+    jz .hlt
+    jmp .print
+
+.hlt:
     mov rax, 0x3c
     syscall
