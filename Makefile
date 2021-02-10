50a91a6541125cddcf5c10f64364b7f8c0517ccc
wf001 <tarkiew@gmail.com>
2021-02-10 20:33:01 +0900

    Add test cases

--- Makefile
+++ Makefile
@@ -5,9 +5,13 @@ xbc : xbc.c
 test:
 	./xbc ws/a.xb
 	./xbc ws/b.xb
+	./xbc ws/c.xb
+	./xbc ws/d.xb
 	./test_xbc.sh
 
 clean:
 	rm -rf xbc
 	rm -rf ws/a
 	rm -rf ws/b
+	rm -rf ws/c
+	rm -rf ws/d
--- test_xbc.sh
+++ test_xbc.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-ope_arr=("a" "b")
-expect_arr=(3 0)
+
+
+ope_arr=("a" "b" "c" "d")
+expect_arr=(3 0 "Hello World!" 8)
 prefix="./ws/"
 
 echo "Starting test."
 echo 
 
-for i in {0..1}; do
+for i in {0..3}; do
     res=$(${prefix}${ope_arr[i]})
     expect=${expect_arr[i]}
 
     echo [${prefix}${ope_arr[i]}]
-    if [ $res -eq $expect ]; then
+    if [[ $res = $expect ]]; then
         echo -e "\e[1;32mPASSED\e[0m : got $res."
     else
         echo -e "\e[1;31mFAILED\e[0m : expected $expect, but got $res."
